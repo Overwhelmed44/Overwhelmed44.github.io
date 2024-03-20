a39ef1fe5b85f46ed13717fdd2858d5462e2ac5a
Gleb <grg0410@mail.ru>
2024-03-20 21:33:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ var heart, write;
 var toWrite = ['Люблю проводить с тобой время', 'Обожаю твои глаза', 'Люблю, когда ты целуешь меня', 'Обожаю твоё лицо', 'Люблю, когда ты улыбаешься', 'Обожаю слушать твои рассказы', 'Люблю лежать с тобой в обнимку', 'Обожаю твой голос'];
 
 async function rewrite() {
-    for (let i = 0;; ++i % toWrite.length) {
-        for (letter of toWrite[i]) {
+    for (let i = 0;; i++) {
+        for (letter of toWrite[i % toWrite.length]) {
             write.innerHTML += letter;
             if (letter != ' ') await new Promise(r => setTimeout(r, 100));
         }
         await new Promise(r => setTimeout(r, 100));
-        for (let j = toWrite[i].length; j >= 0; j--) {
+        for (let j = toWrite[i % toWrite.length].length; j >= 0; j--) {
             write.innerHTML = write.innerHTML.slice(0, j);
             if (toWrite[i][j] != ' ') await new Promise(r => setTimeout(r, 100));
         }
@@ -22,9 +22,9 @@ function pulse() {
 }
 
 function getTimeStamp() {
-    const timeStamp = Math.floor((new Date().getTime() - new Date('2023-11-20').getTime()) / 1000 / 60 / 60 / 24);
+    const timeStamp = Math.floor((new Date().getTime() - new Date('2023-11-20').getTime()) / 1000 / 60 / 60 / 24).toString();
     const last = timeStamp.length - 1;
-    
+
     if ((timeStamp.length >= 2 && timeStamp[last - 1] == '1') || '567890'.match(timeStamp[last])) return `${timeStamp} дней <3`;
     if (timeStamp[last] == '1') return `${timeStamp} день <3`;
     return `${timeStamp} дня <3`;
